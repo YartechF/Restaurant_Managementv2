bd88d9638d7e4303c1772219fa6cfea99b89c3c1
Felbert <felbertyarte6@gmail.com>
2024-07-15 09:24:26 +0800

    .

--- src/main/java/restaurant/controllers/reports_controller.java
+++ src/main/java/restaurant/controllers/reports_controller.java
@@ -1,14 +1,17 @@
 package restaurant.controllers;
 
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.util.StringConverter;
 import restaurant.models.orders_model;
 import restaurant.models.sales_chart_model;
 
@@ -17,19 +20,43 @@ public class reports_controller implements Initializable {
     @FXML
     private BarChart<String, Number> report_chart;
     
-    void setdatachart(){
-        XYChart.Series<String,Number> series = new XYChart.Series<>();
-
-        series.getData().add(new XYChart.Data<String,Number>("May",1000.5));
-        series.getData().add(new XYChart.Data<String,Number>("june",200.0));
-
-        report_chart.getData().add(series);
-    }
+    
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         sales_chart_model SalesChartModel = new sales_chart_model();
-        
-        setdatachart();
-    }
+
+        // Create the axes
+        CategoryAxis xAxis = new CategoryAxis();
+        NumberAxis yAxis = new NumberAxis();
+
+        // Set the axes for the chart
+        report_chart.getXAxis().setLabel("Month");
+        report_chart.getYAxis().setLabel("Sales (After Discount)");
+        report_chart.setBarGap(0.2);
+        report_chart.setLegendVisible(false);
+        report_chart.setTitle("Monthly Sales Report");
+        report_chart.setAnimated(false);
+
+        // Set the categories for the x-axis
+        xAxis.setCategories(FXCollections.observableArrayList(
+            "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        ));
+
+        report_chart.getXAxis().setAnimated(false);
+
+        // Create a data series
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+
+        // Add data to the series
+        try {
+            series.getData().addAll(new sales_chart_model().getdata());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        // Add the data series to the chart
+        report_chart.getData().add(series);
+        }
     
 }
--- src/main/java/restaurant/models/sales_chart_model.java
+++ src/main/java/restaurant/models/sales_chart_model.java
@@ -10,5 +10,42 @@ import javafx.scene.chart.XYChart;
 import restaurant.db.database;
 
 public class sales_chart_model extends database{
+    private ObservableList<XYChart.Data<String, Number>> getOrdersData() throws SQLException {
+        ObservableList<XYChart.Data<String, Number>> data = FXCollections.observableArrayList();
     
+        // Create an instance of your database model class
+        String sql = "SELECT MONTHNAME(i.Date) AS month, SUM(p.price * o.quantity) AS total_sales, AVG(i.discount) AS discount " +
+                     "FROM tbl_orders o " +
+                     "JOIN tbl_product p ON o.productID = p.ID " +
+                     "JOIN tbl_invoice i ON o.invoiceID = i.ID " +
+                     "GROUP BY MONTH(i.Date)";
+
+        PreparedStatement ps = getConnection().prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+    
+        try {
+            double totalYearlySales = 0.0;
+            while (rs.next()) {
+                String month = rs.getString("month");
+                double totalSales = rs.getDouble("total_sales");
+                double discount = rs.getDouble("discount");
+                double salesAfterDiscount = totalSales - (totalSales * discount / 100);
+                totalYearlySales += salesAfterDiscount;
+    
+                data.add(new XYChart.Data<>(month, salesAfterDiscount));
+            }
+    
+            // Add the total yearly sales as a separate data point
+            data.add(new XYChart.Data<>("Total", totalYearlySales));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    
+        return data;
+
+        
+    }
+    public ObservableList<XYChart.Data<String, Number>> getdata() throws SQLException{
+        return this.getOrdersData();
+    }
 }
